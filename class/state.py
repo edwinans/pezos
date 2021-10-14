1df1e3bb5511a69e2df2683a39cf48e20355b41d
Jeremy.Ngu <jeyncsgo@gmail.com>
2021-10-14 16:54:08 +0200

    ficed typo

--- class/state.py
+++ class/state.py
@@ -1,6 +1,7 @@
-
 class State:
-    def __init__(self, dictator_pkey, predecessor_timestamp, nb_bytes_in_next_sequence, accounts):
+    def __init__(
+        self, dictator_pkey, predecessor_timestamp, nb_bytes_in_next_sequence, accounts
+    ):
         self.dictator_pkey = dictator_pkey
         self.predecessor_timestamp = predecessor_timestamp
         self.nb_bytes_in_next_sequence = nb_bytes_in_next_sequence
@@ -8,11 +9,12 @@ class State:
 
 
 class Account:
-    def __init__(self, user_pkey, levelp, timestampp, operations_hashp, context_hashp,signaturep):
+    def __init__(
+        self, user_pkey, levelp, timestampp, operations_hashp, context_hashp, signaturep
+    ):
         self.user_pkey = user_pkey
         self.levelp = levelp
         self.timestampp = timestampp
         self.operations_hashp = operations_hashp
         self.context_hashp = context_hashp
         self.signaturep = signaturep
-    
