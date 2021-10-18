b087c3bd5acba9e4dfc14a73043e487f33d9bb67
Jeremy.Ngu <jeyncsgo@gmail.com>
2021-10-18 16:18:02 +0200

    accounts

--- src/model/state.py
+++ src/model/state.py
@@ -5,15 +5,38 @@ class State:
         self.dictator_pkey = dictator_pkey
         self.predecessor_timestamp = predecessor_timestamp
         self.nb_bytes_in_next_sequence = nb_bytes_in_next_sequence
-        self.accounts = accounts
+        self.accounts = []
 
+        while(cpt < nb_bytes_in_next_sequence):
+            cpt = 0
+
+            userpkey = accounts[cpt:cpt+32]
+            cpt += 32
+
+            predecessorpez = accounts[cpt:cpt+4]
+            cpt += 4
+            
+            timestamppez = accounts[cpt:cpt+4]
+            cpt += 4
+
+            operationhashpez = accounts[cpt:cpt+4]
+            cpt += 4
+
+            contexthashpez = accounts[cpt:cpt+4]
+            cpt += 4
+            
+            signaturepez = accounts[cpt:cpt+4]
+            cpt += 4
+
+            acc = Account(userpkey, predecessorpez, timestampez, operationhashpez, contexthashpez, signaturepez)
+            self.accounts.append[acc]
 
 class Account:
     def __init__(
-        self, user_pkey, levelp, timestampp, operations_hashp, context_hashp, signaturep
+        self, user_pkey, predecessorp, timestampp, operations_hashp, context_hashp, signaturep
     ):
         self.user_pkey = user_pkey
-        self.levelp = levelp
+        self.predecessorp = predecessorp
         self.timestampp = timestampp
         self.operations_hashp = operations_hashp
         self.context_hashp = context_hashp
